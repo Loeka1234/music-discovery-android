0fc89421c43a5e89c96694f4b9277e5dc57f42ae
Loeka Lievens <loekalievens@hotmail.com>
2023-12-27 00:01:35 +0100

    Arrow on card

--- app/src/main/java/com/example/musicdiscovery/ui/screens/shared/ArtistCard.kt
+++ app/src/main/java/com/example/musicdiscovery/ui/screens/shared/ArtistCard.kt
@@ -5,9 +5,13 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowRight
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
@@ -42,16 +46,22 @@ fun ArtistCard(
                 )
         }
 
-        if (favoriteArtist != null)
-            Row(
-                horizontalArrangement = Arrangement.End,
-                modifier = Modifier.fillMaxWidth()
-            ) {
+        Row(
+            horizontalArrangement = Arrangement.End,
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            if (favoriteArtist != null)
                 FavoriteButton(
                     isFavorite = isFavorite,
                     onFavoriteClick = favoriteArtist,
-                    modifier = Modifier.padding(8.dp)
                 )
-            }
+
+            Icon(
+                Icons.Filled.KeyboardArrowRight,
+                contentDescription = "Arrow right",
+                modifier = Modifier.padding(end = 8.dp)
+            )
+        }
     }
 }
--- app/src/main/java/com/example/musicdiscovery/ui/screens/shared/CustomCard.kt
+++ app/src/main/java/com/example/musicdiscovery/ui/screens/shared/CustomCard.kt
@@ -8,8 +8,13 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.FavoriteBorder
+import androidx.compose.material.icons.filled.KeyboardArrowRight
 import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
